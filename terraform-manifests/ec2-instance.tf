e4e65b339f47bf8bb6ca171aefbb7734d93c5bda
Raju Kalapala <rajuaws8242@gmail.com>
2021-10-17 18:39:56 +0800

    Initial files

---
+++ terraform-manifests/ec2-instance.tf
@@ -0,0 +1,22 @@
+# Terraform Settings Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      #version = "~> 3.21" # Optional but recommended in production
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  region  = "us-east-1"
+}
+
+# Resource Block
+resource "aws_instance" "ec2demo" {
+  #ami           = "ami-0be2609ba883822ec" # Amazon Linux in us-east-1, update as per your region
+  ami           = "ami-0dc2d3e4c0f9ebd18" # Amazon Linux in us-east-1, update as per your region
+  instance_type = "t2.micro"
+}
